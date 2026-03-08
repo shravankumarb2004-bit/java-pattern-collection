746c2f0d19cb13171a7c1a240ec2632f71b13aca
the Shravan <shravankumarb2004@gmail.com>
2026-03-08 10:08:07 +0530

    Add files via upload

---
+++ src/SolidRect.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class SolidRect {
+    static void main(String[] args) {
+        int n=4;
+        int m=5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
