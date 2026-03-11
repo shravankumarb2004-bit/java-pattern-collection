7222ec51036f2b034ff46e99868011f276469bd7
the Shravan <shravankumarb2004@gmail.com>
2026-03-11 19:40:38 +0530

    Add files via upload

---
+++ src/Solid_Rhombus.java
@@ -0,0 +1,17 @@
+package patterns;
+
+public class Solid_Rhombus {
+    static void main(String[] args) {
+        int n=6;
+
+        for(int i=1;i<=n;i++){
+            for(int j=(n-i)+1;j>=1;j--){
+                System.out.print("  ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
